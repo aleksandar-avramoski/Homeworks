5e4576212fe101d99b5606cf620e37d3e575ee41
Aleksandar Avramoski <avramoskia04@gmail.com>
2025-04-15 15:20:28 +0200

    Delete Homework-8 directory

--- Homework-8/script.js
+++
@@ -1,8 +0,0 @@
-let priceOfPhone = 119.95;
-let taxRate = 0.05;
-let taxAmount = priceOfPhone * taxRate;
-let totalPriceOfPhone = priceOfPhone + taxAmount;
-let priceOfAllPhones = totalPriceOfPhone * 30;
-
-console.log("Price of all 30 phones:")
-console.log(priceOfAllPhones);
